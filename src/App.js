a5bfc89de446a7bab3f9483d3b36ed40738d8ed7
sdirbingakkum <sdirbingakkum@gmail.com>
2025-10-13 14:47:04 +0700

    Update App.js
    enables satlak create

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import PenggunaList from "./resources/pengguna/PenggunaList";
 import auth from "./providers/auth";
 import PemohonList from "./resources/pemohon/PemohonList";
 import route from "./providers/route";
+import SatlakCreate from "./resources/satlak/SatlakCreate";
 import SatlakEdit from "./resources/satlak/SatlakEdit";
 import PenggunaEdit from "./resources/pengguna/PenggunaEdit";
 import PemohonEdit from "./resources/pemohon/PemohonEdit";
@@ -65,6 +66,7 @@ const App = () => {
               ? SatlakCreate
               : null
           }
+          edit={SatlakCreate}
           edit={SatlakEdit}
           list={SatlakList}
         />,
