972d64aff37fd503446439e60bc1a1370a4ebd23
sdirbingakkum <sdirbingakkum@gmail.com>
2025-12-21 17:59:37 +0700

    Update SimCanvasDepan.js

--- src/resources/sim/print/SimCanvasDepan.js
+++ src/resources/sim/print/SimCanvasDepan.js
@@ -588,8 +588,8 @@ const SimCanvasDepan = forwardRef((props, ref) => {
             image={sidik_jari_pemohon}
             x={10}
             y={85}
-            scaleY={0.20}
-            scaleX={0.20}
+            scaleY={0.15}
+            scaleX={0.15}
           />
         )}
 
